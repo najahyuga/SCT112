a8ca855987aaad4e2246805bcfa9ec10016736e7
najahyuga <najahyugad@gmail.com>
2023-02-06 10:04:22 +0700

    Update features delete akun in ProfilActivity.java

--- app/src/main/java/com/example/sct112/ProfilActivity.java
+++ app/src/main/java/com/example/sct112/ProfilActivity.java
@@ -1,17 +1,72 @@
 package com.example.sct112;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ProfilActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+    private Button btnDelete, btnHome;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+        btnDelete = (Button) findViewById(R.id.buttonDelete);
+        btnHome = (Button) findViewById(R.id.buttonBackHome);
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnDelete();
+            }
+        });
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ProfilActivity.this, LoginActivity.class));
+            }
+        });
+    }
+
+    private boolean btnDelete() {
+        user.delete()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Log.d(TAG, "Delete Akun Sukses");
+                        }else{
+                            Log.d(TAG, "Delete Akun GAGAL");
+                        }
+                    }
+                });
+
+        DatabaseReference deleteReference = FirebaseDatabase.getInstance()
+                .getReference("Users")
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+        deleteReference.removeValue();
+        Toast.makeText(ProfilActivity.this, "Delete Akun", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(this, LoginActivity.class));
+        return true;
     }
 
     public void SwipeProfile(View view) {
